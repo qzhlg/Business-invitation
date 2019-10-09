0477dc8cc547762b482037b09c52248652da5d37
qzhlg <zhl042768g@163.com>
2019-10-09 18:34:00 +0800

    '初始化'

---
+++ src/store/modules/entrepreneurship.js
@@ -0,0 +1,20 @@
+const state = {
+    list:[]
+  }
+  
+  const mutations = {
+ 
+  }
+  
+  const actions = {
+    
+  
+  }
+  
+  export default {
+    namespaced: true,
+    state,
+    mutations,
+    actions
+  }
+  
---
+++ src/views/entrepreneurship/entrepreneurs-invest-list.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <div class="selectbox">select</div>
+    <div class="btnbox">
+      <div class="leftbtn">
+        <button>+新增用户</button>
+        <button>今日跟进</button>
+      </div>
+      <div class="rightbtn">
+        <button>搜索</button>
+        <button>重置</button>
+      </div>
+    </div>
+    <div class="tablebox">table</div>
+  </div>
+</template>
+<script>
+export default {
+    
+};
+</script>
+<style scoped>
+.selectbox{
+    height: auto;
+
+}
+.tablebox{
+    height: auto;
+}
+</style>
